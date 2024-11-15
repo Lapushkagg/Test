18a5bc8b60a2952770e76a13408f89e5be268d7a
Lapushkagg <Lapushkagg8@gmail.com>
2024-11-15 15:32:41 +0300

    Add files via upload

---
+++ 03_lesson_dz/lesson_3_task_1.py
@@ -0,0 +1,9 @@
+from user import User
+
+my_user=User("Алексей","Петров")
+
+
+my_user.print_name()
+my_user.print_Suname()
+my_user.print_FIO()
+
---
+++ 03_lesson_dz/smartphone.py
@@ -0,0 +1,6 @@
+class Smartphone:
+  def __init__(self, marka, madel,nomer):
+    self.marka = marka
+    self.madel = madel
+    self.nomer = nomer
+
