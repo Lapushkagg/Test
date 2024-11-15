c4363a3d60d8cf3cf04408f8b094f2810fc167d2
Lapushkagg <Lapushkagg8@gmail.com>
2024-11-15 17:17:23 +0300

    Add files via upload

---
+++ 03_lesson_dz/lesson_3_task_2.py
@@ -0,0 +1,14 @@
+from smartphone import Smartphone
+
+catalog = [
+    Smartphone("iphone", "se", 89034324234),
+    Smartphone("iphone", "8+", 567567567),
+    Smartphone("iphone", "14 pro", 3453453345),
+    Smartphone("xuawau", "12s", 8903324234), 
+    Smartphone("iphone", "15", 67567567567),
+    Smartphone("pocp", "8", 12334435)
+]
+
+# Печатаем библиотеку
+for Smartphone in catalog:
+    print(f"{Smartphone.marka} - {Smartphone.madel}. {Smartphone.nomer}")
---
+++ 03_lesson_dz/smartphone.py
@@ -0,0 +1,6 @@
+class Smartphone:
+  def __init__(self, mark, model,nomber):
+    self.mark = mark
+    self.model = model
+    self.nomber = nomber
+
